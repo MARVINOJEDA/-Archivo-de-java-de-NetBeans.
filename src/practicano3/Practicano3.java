39b873bf9c0e28765d73a5a40b682a6ff4fd83b0
victor <victor@DESKTOP-RPUQUHF>
2023-11-04 20:06:57 -0600

    commit 2 Signed-off-by:victor <victor@DESKTOP-RPUQUHF>

---
+++ src/practicano3/Practicano3.java
@@ -0,0 +1,27 @@
+/*Marvin Rosas Ojeda
+Numero de control: 23580069
+29/10/2023
+Practica No.3
+Mostrar un mensaje sobre los dias laborales y dias no laborales.
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicano3;
+
+/**
+ *
+ * @author Alumnos
+ */
+public class Practicano3 {
+static final int diasLaborales=5;
+static final int diasNoLaborales=2;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println ("Numero de dias laborales" + diasLaborales);
+        System.out.println("Numero de dias no laborales" + diasNoLaborales);
+    }
+    
+}
